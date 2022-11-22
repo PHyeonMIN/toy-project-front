77be2c66149f0eca1574bf65d7078079c40080b2
박현민 <dndb4902@naver.com>
2022-11-22 10:08:42 +0900

    # babel & webpack & esLint 설정 (추가예정)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+const widthBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+const webpack = require('webpack');
+
+dotenvLoad();
+
+const withNextEnv = nextEnv();
+
+module.exports = {
+  webpack(config ,{ isServer, buildId} ) {
+    config.resolve.modules.push(__dirname)
+
+    return config;
+  }
+};
