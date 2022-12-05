bb96617f9e68f444192fe2b5ccdb8e88319956f3
박현민 <dndb4902@naver.com>
2022-12-05 18:05:33 +0900

    # webpack config start?

--- next.config.js
+++ next.config.js
@@ -4,21 +4,27 @@
 //   swcMinify: true,
 // }
 
-const widthBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-const nextEnv = require('next-env');
-const dotenvLoad = require('dotenv-load');
-const webpack = require('webpack');
-
-dotenvLoad();
-
-const withNextEnv = nextEnv();
-
+// const widthBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true',
+// });
+// const nextEnv = require('next-env');
+// const dotenvLoad = require('dotenv-load');
+// const webpack = require('webpack');
+//
+// dotenvLoad();
+//
+// const withNextEnv = nextEnv();
+//
+// module.exports = {
+//     webpack(config, { isServer, buildId }) {
+//         config.resolve.modules.push(__dirname);
+//
+//         return config;
+//     },
+// };
 module.exports = {
-  webpack(config ,{isServer, buildId} ) {
-    config.resolve.modules.push(__dirname);
-
-    return config;
-  },
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+    ) => config,
 };
